c391fa606a2732ba2eaebfd0a97875c14c1d26c2
viwo70199 <susiloprojekc@gmail.com>
2026-03-14 20:55:53 +0700

    Add app/src/main/java/com/anjas/facebook/MainActivity.java

---
+++ app/src/main/java/com/anjas/facebook/MainActivity.java
@@ -0,0 +1,18 @@
+package com.anjas.facebook;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://mbasic.facebook.com");
+        setContentView(webView);
+    }
+}
